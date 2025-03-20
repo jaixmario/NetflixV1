7210dbb5cffaf8f5ee84a9aa1491618fa7498ea4
jaideep <135174306+Niosjai@users.noreply.github.com>
2025-03-21 00:56:44 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -4,7 +4,7 @@ import os
 import json
 from datetime import datetime
 
-UPLOAD_INTERVAL = 10  # 1 hour
+UPLOAD_INTERVAL = 3600 # 1 hour
 BACKUP_FOLDER_ID = "01ZDEC6CXCKCNDJGM7X5EK33GFMY6DW5TL"  # OneDrive folder ID
 TOKEN_FILE = "user_token.json"  # Store the access token here
 
